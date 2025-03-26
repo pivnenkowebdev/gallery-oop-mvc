a5ee0e2e539aa1c9611d309faf2490e049ead2fd
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-03-26 22:40:09 +0300

    feat: add btns params

--- src/components/list-buttons-params.js
+++ src/components/list-buttons-params.js
@@ -5,4 +5,115 @@ const listButtonsParams = {
   },
 };
 
-export { listButtonsParams };
+const btnsParams = [
+  {
+    tagName: "button",
+    text: "Россия",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "russia.svg",
+  },
+  {
+    tagName: "button",
+    text: "ישראל",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "israel",
+  },
+  {
+    tagName: "button",
+    text: "საქართველო",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "georgia",
+  },
+  {
+    tagName: "button",
+    text: "日本",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "japan",
+  },
+  {
+    tagName: "button",
+    text: "Україна",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "ukraine",
+  },
+  {
+    tagName: "button",
+    text: "Great Britain",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "britain",
+  },
+  {
+    tagName: "button",
+    text: "भारत",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "india",
+  },
+  {
+    tagName: "button",
+    text: "한국",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "korea",
+  },
+  {
+    tagName: "button",
+    text: "Türkiye",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "turkey",
+  },
+  {
+    tagName: "button",
+    text: "中國",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "china",
+  },
+  {
+    tagName: "button",
+    text: "مصر",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "egypt",
+  },
+  {
+    tagName: "button",
+    text: "Ελλάδα",
+    attr: {
+      "data-btn": "",
+    },
+    classList: ["btnAction"],
+    imgSrc: "greece",
+  },
+];
+
+export { listButtonsParams, btnsParams };
