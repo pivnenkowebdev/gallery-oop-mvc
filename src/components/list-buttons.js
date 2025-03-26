34504c77140f3a5d46137a351176c854979a2ed5
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-03-26 22:53:03 +0300

    feat: add actions btns

--- src/components/list-buttons.js
+++ src/components/list-buttons.js
@@ -1,8 +1,20 @@
 import Creator from "../utilities/creator.js";
-import { listButtonsParams } from "./list-buttons-params.js";
+import { btnsParams, listButtonsParams } from "./list-buttons-params.js";
 
 export default class ListButtons extends Creator {
   constructor() {
     super(listButtonsParams);
+    this.childs = [];
+    this.#createItems();
+    this._addChildsElement();
+  }
+
+  #createItems() {
+    btnsParams.forEach((itemParams) => {
+      const item = document.createElement("li");
+      const btn = new Creator(itemParams);
+      item.append(btn.getElement());
+      this.childs.push(item);
+    });
   }
 }
