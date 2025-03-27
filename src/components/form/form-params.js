1209f8f523d3608a7c32d67d4609e9ca955698df
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-03-27 10:38:17 +0300

    feat: add form component

---
+++ src/components/form/form-params.js
@@ -0,0 +1,19 @@
+const formParams = {
+  tagName: "form",
+  classList: ["form"],
+};
+
+const inputParams = {
+  tagName: "input",
+  classList: ["input"],
+};
+
+const btnSubmitParams = {
+  tagName: "input",
+  classList: ["input"],
+  attr: {
+    type: "submit",
+  },
+};
+
+export { formParams, inputParams, btnSubmitParams };
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import Creator from "../../utilities/creator.js";
+import Form from "../form/form.js";
 import ListButtons from "../list-btns/list-buttons.js";
 import { headerParams } from "./header-params.js";
 
@@ -6,7 +7,8 @@ export default class Header extends Creator {
   constructor() {
     super(headerParams);
     this.listButtons = new ListButtons();
-    this.childs = [this.listButtons];
+    this.form = new Form();
+    this.childs = [this.listButtons, this.form];
     this._addChildsElement();
   }
 }
